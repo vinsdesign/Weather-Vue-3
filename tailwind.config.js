500a5dbd797e5f2f669185534b765914a9500172
evinsdesign <kelvinsaputra024@gmail.com>
2024-08-18 17:22:05 +0800

    setup vue and tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,17 @@ export default {
         PoppinsMedium: "Poppins-Medium, sans-serif",
         Poppins: "Poppins-Regular, sans-serif",
       },
+      screens: {
+        xxsm: '320px',
+        // => @media (min-width: 320px) { ... }
+        xsm: '370px',
+        // => @media (min-width: 370px) { ... }
+        sm: '640px',
+        // => @media (min-width: 640px) { ... }
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
     },
   },
   plugins: [],
