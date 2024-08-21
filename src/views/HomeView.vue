586b3cddeb06b6de783bb3c11cbba737ebd9a24d
evinsdesign <kelvinsaputra024@gmail.com>
2024-08-21 23:02:52 +0800

    feat: searchBar City Search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import SiteNavigation from "@/components/SiteNavigation.vue";
+import SearchBar from "@/components/SearchBar.vue";
 </script>
 
 <template>
   <SiteNavigation />
+  <SearchBar />
 </template>
